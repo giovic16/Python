b83cea176c450f78d093be66c05156f5fbac3ce7
Giovana <giovanavictoria887@gmail.com>
2020-05-14 20:10:22 -0300

    solo6.py

---
+++ solo6.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = int(input('Digite o raio do circulo:'))
+print('A area do circulo é igual a:' , (raio**2)*3.14//1)
