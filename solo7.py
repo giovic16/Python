fc513fc05615f4de10c95a7c318d982f13c74c91
Giovana <giovanavictoria887@gmail.com>
2020-05-14 20:12:10 -0300

    solo7.py

---
+++ solo7.py
@@ -0,0 +1,5 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+l = int(input('Qual a área do quadrado?'))
+h = (l**2)
+dobro = (l**2)*2
+print('A área é igual a {} e o dobro da área é {}' .format(h, dobro))
